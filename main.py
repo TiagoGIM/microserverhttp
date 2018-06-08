5e86cc51fd2ef9397851a98a14a27c13a345b4b2
Jmathbr <joao.matheus92225@gmail.com>
2018-06-07 22:32:02 -0300

    metodo tag add

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ from uconnect import uConnect
 import time
 cn = uConnect()
 cn.wlan_manual()
-
 rf = RfidPorteiro()
-tags = ["521252194","212516444","211595197"]
+
 while(True):
 
     tag = str(rf.get())
     time.sleep_ms(100)
+
     if tag != "SemTag":
         print(tag)
         print ("okok")
         url = "http://10.6.3.114:8012/?Matricula="+tag
+        #url = "http://10.6.3.114:8012/?Tag="+tag
         response = urequests.get(url)
